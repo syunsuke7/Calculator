9236d4ddcb4af613f3841614d717467cec51f510
小田隼輔 <odashuntasuku@odashuntasukunoMacBook-Pro.local>
2026-01-14 13:28:59 +0900

    最初のコミット

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
 import logo from './logo.svg';
 import './App.css';
+import { use, useState } from 'react';
 
-function App() {
+export function App() {
+  const [num, setNum] = useState("");
+  const [keisanNum, setKeisanNum] = useState(num);
+  const keisanShiki = ["+", "-", "/" ,"*"]
+
+  const onNumberClick = (digit) => {
+    setNum((prevNum) => {
+      if (prevNum === "" && digit === "0"){
+        return "";
+      }
+      if (prevNum.includes(" = ")){
+        return digit === "0" ? "" : digit;
+      }else{
+        if (digit === "0" && keisanShiki.includes(prevNum.slice(-1))){
+          return prevNum;
+        }else{
+          return prevNum + digit;
+        }
+      }
+    })
+    setKeisanNum((prevKeisanNum) => {
+      if (prevKeisanNum === "" && digit === "0"){
+        return "";
+      }
+      if (prevKeisanNum.includes(" = ")){
+        return digit === "0" ? "" : digit;
+      }else{
+        if ( digit === "0" && keisanShiki.includes(prevKeisanNum.slice(-1))){
+          return prevKeisanNum;
+        }else{
+          return prevKeisanNum + digit;
+        }
+      }
+    })
+  }
+
+  const onClearClick = () => {
+    setNum("");
+    setKeisanNum("");
+  }
+  
+  const onKeisanClick = (shiki) => {
+    const newKeisanNum = num + shiki;
+    setNum((prevNum) => {
+      if (prevNum.includes("=") || prevNum === ""){
+        return "";
+      }else{
+        return newKeisanNum;
+      }
+    })
+    setKeisanNum((prevKeisanNum) => {
+      if (prevKeisanNum.includes("=") || prevKeisanNum === ""){
+        return "";
+      }else{
+        return newKeisanNum;
+      }
+    })
+  }
+
+  const onTotalClick = () => {
+    if(keisanShiki.includes(keisanNum.slice(-1))){
+      return setNum(keisanNum + " = エラー" );
+    }
+    const newTotalNum = eval(keisanNum);
+    setNum(keisanNum + " = " + newTotalNum);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="total-area">
+        <p className="total">計算結果</p>
+        <p className="total-num">{num == "" ? "0" : num}</p>
+      </div>
+      <div className="KeisanStyle">
+        <div className="suzi">
+          <button className="suziSingel" onClick={() => {onNumberClick("1")}}>1</button>
+          <button className="suziSingel" onClick={() => {onNumberClick("2")}}>2</button>
+          <button className="suziSingel" onClick={() => {onNumberClick("3")}}>3</button>
+        </div>
+        <div className="suzi">
+          <button className="suziSingel" onClick={() => {onNumberClick("4")}}>4</button>
+          <button className="suziSingel" onClick={() => {onNumberClick("5")}}>5</button>
+          <button className="suziSingel" onClick={() => {onNumberClick("6")}}>6</button>
+        </div>
+        <div className="suzi">
+          <button className="suziSingel" onClick={() => {onNumberClick("7")}}>7</button>
+          <button className="suziSingel" onClick={() => {onNumberClick("8")}}>8</button>
+          <button className="suziSingel" onClick={() => {onNumberClick("9")}}>9</button>
+        </div>
+        <div className="suzi">
+          <button className="suziSingel" onClick={() => {onNumberClick("0")}}>0</button>
+        </div>
+        <div className="keisan-shiki">
+          <button className="suziSingel" onClick={() => onClearClick()}>C</button>
+          <button className="suziSingel" onClick={() => onKeisanClick("+")}>+</button>
+          <button className="suziSingel" onClick={() => onKeisanClick("-")}>-</button>
+          <button className="suziSingel" onClick={() => onKeisanClick("/")}>/</button>
+          <button className="suziSingel" onClick={() => onKeisanClick("*")}>*</button>
+          <button className="suziSingel" onClick={() => onTotalClick()}>=</button>
+        </div>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
